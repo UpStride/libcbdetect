237baa91b73c666e47b2af90fa14f1eb2724d739
Philipe Moura <philipe.moura@upstride.io>
2021-06-17 14:38:52 +0200

    Casted checkerboards into Numpy arrays at the C++ level.

--- example.py
+++ example.py
@@ -2,6 +2,5 @@ import cbdetect_python as cb
 import numpy as np
 
 checkerboards = cb.detect('example_data/e2.png')
-checkerboards = [np.asarray(checkerboard) for checkerboard in checkerboards]
-print(f'There is a total of {len(checkerboards)} detected in the input image.')
+print(f'There is a total of {len(checkerboards)} checkerboards detected in the input image.')
 print(f'The shape of the first checkerboard is {checkerboards[0].shape}.')
--- src/bind.cc
+++ src/bind.cc
@@ -10,10 +10,9 @@
 namespace py = pybind11;
 
 
-std::vector<std::vector<std::vector<std::vector<double>>>> detect(const char* filename) {
-    std::vector<std::vector<std::vector<std::vector<double>>>> checkerboards;
-    std::vector<std::vector<std::vector<double>>> checkerboard;
-    std::vector<std::vector<double>> checkerboard_row;
+std::vector<py::array_t<double>> detect(const char* filename) {
+    std::vector<py::array_t<double>> output;
+    py::array_t<double> boardPyArray;
 
     cbdetect::Corner corners;
     std::vector<cbdetect::Board> boards;  
@@ -35,30 +34,50 @@ std::vector<std::vector<std::vector<std::vector<double>>>> detect(const char* fi
 
     // loop boards
     for (const auto& board: boards) {
+        int j = -1;
+        size_t h = board.idx.size() - 2;
+        size_t w = board.idx[0].size() - 2;
+        size_t size = h*w*2;
+        double *points = new double[size];
+
         // loop board rows
         for (const auto& row: board.idx) {
+            int k = 0;
+
             // loop row indices
             for (const int i: row) {
                 if (i < 0) {
                     continue;
                 }
                 const cv::Point2d corner(corners.p[i]);
-                checkerboard_row.push_back({corner.x, corner.y});
+                points[j*2*w + 2*k] = (double) corner.x;
+                points[j*2*w + 2*k + 1] = (double) corner.y;
+                k++;
             }
-            if (checkerboard_row.size() != 0) {
-                checkerboard.push_back(checkerboard_row);
-                checkerboard_row.clear();
-            }
+            j++;
         }
-        checkerboards.push_back(checkerboard);
-        checkerboard.clear();
+
+        // Create a Python object that will free the allocated
+        // memory when destroyed:
+        py::capsule free_when_done(points, [](void *f) {
+            double *points = reinterpret_cast<double *>(f);
+            delete[] points;
+        });
+
+        // Cast to py:array so that is interpreted as Numpy array in Python. Explanation:
+        // https://github.com/pybind/pybind11/issues/1299#issuecomment-368815504
+        boardPyArray = py::array_t<double>(std::vector<ptrdiff_t>{(long int)h, (long int)w, 2},
+                                                 &points[0]);
+        output.push_back(boardPyArray);
     }
-    return checkerboards;
+    return output;
 }
 
 
 PYBIND11_MODULE(cbdetect_python, m) {
     m.doc() = "libcbdetect Python binding";
 
-    m.def("detect", &detect, pybind11::return_value_policy::take_ownership);
+    m.def("detect", &detect, "Returns a list with N 3D Numpy arrays, where N is the number of\
+checkerboards detected. The two outermost dimensions of the array correspond to the height and width\
+indexes of the checkerboard intersection and the innermost dimension is its pixel coordinates.");
 }
